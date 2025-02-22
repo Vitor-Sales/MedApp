45f31b33e75570ba97398440e172ec9b1f6a374e
Vitor Sales <vitormalisa@gmail.com>
2025-02-22 18:59:06 -0300

    feat: create routes

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,8 @@
 import { express } from "express";
+import appointmentController from "./AppointmentController";
+import doctorController from "./DoctorController";
+import pacientController from "./PacientController";
+import prescriptionController from "./PrescriptionController";
 
 let router = express.Router();
 
@@ -8,5 +12,10 @@ router.get(
     res.status(200).json({ message: "Hi!"});
   }
 );
+
+router.use("/", appointmentController);
+router.use("/", pacientController);
+router.use("/", prescriptionController);
+router.use("/", doctorController);
 
 export default router;
